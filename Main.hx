6846c830d6cdc8a931ce7a35ceed027b739ac813
zelimirus <stojakovic.net@gmail.com>
2016-06-20 23:12:55 +0200

    new start

--- Main.hx
+++ Main.hx
@@ -1,56 +1,107 @@
-
-import pixi.core.graphics.Graphics;
+import pixi.interaction.EventTarget;
+import pixi.core.sprites.Sprite;
 import pixi.core.display.Container;
 import pixi.core.textures.Texture;
-import pixi.core.renderers.SystemRenderer;
-import pixi.core.renderers.Detector;
-import pixi.core.sprites.Sprite;
+import pixi.plugins.app.Application;
+import pixi.extras.TilingSprite;
 import js.Browser;
 
-class Main {
+class Main extends Application {
 
-	var _bunny:Sprite;
-	var _renderer:SystemRenderer;
-	var _container:Container;
-
-	var _graphic:Graphics;
+	var texture1:Texture;
+	var gameStateZERO           = 0;
+    var gameStateINIT           = 1;
+    var gameStateMOVING         = 2;
+    var gameStateCHECK_WIN      = 3;
+    var slotNumber              = 5;
+    var initalX                 = 395;
+    var tileHEIGHT              = 100;
+    var tileWIDTH               = 100;
+    var nCycly                  = 5;
+    var tTiles                  = 7;
+    var selectCandidateInitalY  = 535;
+    var selectCandidateInitalX  = 380;
+    var selectCandidateHight    = 70;
+    var selectCandidateWidth    = 70;
+    var volume                  = 1;
+    var creditValue             = 5000;
+    var result                  = [];
+    var gameStatus              = 0;
+    var finalTileY1             = [];
+    var finalTileY2             = [];
+    var finalTileY3             = [];
+    var slotSprite1             = [];
+    var slotSprite2             = [];
+    var slotSprite3             = [];
+    var preChoosedPosition1 = [0, 2, 4, 6, 1];
+    var preChoosedPosition2 = [2, 0, 6, 3, 4];
+    var preChoosedPosition3 = [6, 1, 2, 4, 0];
+    var yourCandidate           = 0;
+    var finalCandidate          = '';
+    var invested                = 0;
+    var isAnimated              = true;
+    var _width                   = 1300;
+    var _height                  = 650;
+    var amount                  = 20;
+    var dolar                   = [];
+    var gravity                 = 0.5;
+    var maxX                    = 1300;
+    var minX                    = 0;
+    var maxY                    = 650;
+    var minY                    = 0;
+    var startDolarCount         = 0;
+    var count                   = 0;
+    var i = 0;
 
 	public function new() {
-		// Rendering options usage sample
-		var options:RenderingOptions = {};
-		options.backgroundColor = 0x006666;
-		options.resolution = 1;
-		options.transparent = true;
-		options.antialias = false;
-
-		_container = new Container();
-		_renderer = Detector.autoDetectRenderer(800, 600, options);
-
-		_bunny = new Sprite(Texture.fromImage("assets/basics/bunny.png"));
-		_bunny.anchor.set(0.5);
-		_bunny.position.set(400, 300);
-
-		_container.addChild(_bunny);
-
-		_graphic = new Graphics();
-		_graphic.beginFill(0xFF0000, 0.4);
-		_graphic.drawRect(200, 150, 400, 300);
-		_graphic.endFill();
-
-		_graphic.interactive = true;
-		_graphic.on("click", function(evt) {trace(evt);});
-
-		_container.addChild(_graphic);
-
-		Browser.document.body.appendChild(_renderer.view);
-		Browser.window.requestAnimationFrame(cast _animate);
+		super();
+		_init();
 	}
 
-	function _animate() {
-		Browser.window.requestAnimationFrame(cast _animate);
-		_bunny.rotation += 0.1;
-		_renderer.render(_container);
+	function _init() {
+		backgroundColor = 0xE0E6F8;
+	
+		super.start();
+
+		
+		texture1 = Texture.fromImage("assets/images/slot3.png");
+		
+
+
+    while (i < slotNumber) {
+        slotSprite1[i] = new TilingSprite(texture1,tileHEIGHT,tileWIDTH);
+        slotSprite1[i].tilePosition.x   = 0;
+        slotSprite1[i].tilePosition.y   = (-preChoosedPosition1[i] * 100);
+        slotSprite1[i].x                = initalX + (i * 105);
+        slotSprite1[i].y                = 105;
+        stage.addChild(slotSprite1[i]);
+        i++;
+    }
+    i = 0;
+     while (i < slotNumber) {
+        slotSprite2[i] = new TilingSprite(texture1,tileHEIGHT,tileWIDTH);
+        slotSprite2[i].tilePosition.x   = 0;
+        slotSprite2[i].tilePosition.y   = (-preChoosedPosition2[i] * 100);
+        slotSprite2[i].x                = initalX + (i * 105);
+        slotSprite2[i].y                = 210;
+        stage.addChild(slotSprite2[i]);
+        i++;
+    }
+    i = 0;
+     while (i < slotNumber) {
+        slotSprite3[i] = new TilingSprite(texture1,tileHEIGHT,tileWIDTH);
+        slotSprite3[i].tilePosition.x   = 0;
+        slotSprite3[i].tilePosition.y   = (-preChoosedPosition3[i] * 100);
+        slotSprite3[i].x                = initalX + (i * 105);
+        slotSprite3[i].y                = 315;
+        stage.addChild(slotSprite3[i]);
+        i++;
+    }
+
+
 	}
+
+	
 
 	static function main() {
 		new Main();
